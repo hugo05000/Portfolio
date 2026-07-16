ebeefc393ab1492aa3bd970ee2b4f83467bc74af
hugomarceau <marceauhugo@gmail.com>
2026-07-16 08:08:56 +0200

    Remise à propre

--- app/Http/Controllers/front/HomeController.php
+++ app/Http/Controllers/front/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\front;
 
 use App\Http\Controllers\Controller;
 use App\Models\Client;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
 
 class HomeController extends Controller
 {
